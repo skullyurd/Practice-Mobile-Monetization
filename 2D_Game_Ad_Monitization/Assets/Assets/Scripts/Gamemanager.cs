4fa9030e64815ff33b2d70d678138940c61c4fdc
skullyurd <47215203+skullyurd@users.noreply.github.com>
2022-01-11 17:43:07 +0100

    Project set-up
    Set the project up, i'm pushing this for back-up purposes.

---
+++ 2D_Game_Ad_Monitization/Assets/Assets/Scripts/Gamemanager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+    [SerializeField] private int curClicks;
+
+    [SerializeField] public Gamemanager gm;
+
+
+    private void Awake()
+    {
+        
+    }
+}
